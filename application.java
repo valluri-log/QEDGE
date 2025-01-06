35e6e71023cf4eac6d9dfbcda42b939137611928
valluri-log <vallurirajesh634@gmail.com>
2025-01-06 16:23:36 +0530

    Update application.java

--- application.java
+++ application.java
@@ -1,3 +1,5 @@
+
+
 apply plugin:'application'
 mainClassName = 'HelloWorld'
 
@@ -11,4 +13,15 @@ repositories {
 dependencies {
     // add dependencies here as below
     implementation group: 'org.apache.commons', name: 'commons-lang3', version: '3.9'
+}
+
+webBook Job Demo
+    public class Greeting {
+    static void hello() {
+        System.out.println("Hello.. Happy learning!");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
 }
